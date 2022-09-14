16cfce92bc2f5dcfcad0486c598e198b804677b4
Julien Scourneau <scourneaujulien@outlook.com>
2022-09-14 14:37:10 +0200

    Update form, added insert in php

--- assets/php/index.php
+++ assets/php/index.php
@@ -1,1 +1,25 @@
 <?php
+include 'database.php';
+header("location: ../../index.html");
+$name = htmlspecialchars($_POST['name']);
+$firstname = htmlspecialchars($_POST['firstname']);
+$mail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$file = $_POST['file'];
+$description = htmlspecialchars($_POST['description']);
+
+$mail = filter_var($mail, FILTER_VALIDATE_EMAIL);
+
+
+$query = "INSERT INTO `contact`(`name`, `firstname`, `mail`, `file`, `description`) VALUES(?,?,?,?,?)";
+if (!empty($db)) {
+    $insert = $db->prepare($query);
+    $insert->execute(array(
+        $name,
+        $firstname,
+        $mail,
+        $file,
+        $description
+    ));
+
+    exit();
+}
