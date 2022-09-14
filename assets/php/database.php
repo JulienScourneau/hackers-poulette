6c06920f95a08f7c9864a6e7049fde6f57830522
Julien Scourneau <scourneaujulien@outlook.com>
2022-09-14 10:31:00 +0200

    Added db connection

--- assets/php/database.php
+++ assets/php/database.php
@@ -1,1 +1,12 @@
 <?php
+$user = "root";
+$password = "";
+$database = "mysql:host=localhost;dbname=hackers-poulette;charset=utf8";
+$db= null;
+
+try {
+    $db = new PDO($database,$user,$password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e){
+    echo $e->getMessage();
+}
