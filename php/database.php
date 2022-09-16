05898db8a6a432d0559b0e4d52a4cbd15686b61d
Julien Scourneau <scourneaujulien@outlook.com>
2022-09-16 11:43:45 +0200

    Added insert validation with rakit

--- php/database.php
+++ php/database.php
@@ -1,12 +1,16 @@
 <?php
-$user = "root";
-$password = "";
-$database = "mysql:host=localhost;dbname=hackers-poulette;charset=utf8";
-$db= null;
 
-try {
-    $db = new PDO($database,$user,$password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e){
-    echo $e->getMessage();
+function getDbconnection(){
+    $user = "root";
+    $password = "";
+    $database = "mysql:host=localhost;dbname=hackers-poulette;charset=utf8";
+    $db= null;
+
+    try {
+        $db = new PDO($database,$user,$password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e){
+        echo $e->getMessage();
+    }
+    return $db;
 }
--- php/insert.php
+++ php/insert.php
@@ -1,28 +1,62 @@
 <?php
+require('../vendor/autoload.php');
+
+use Rakit\Validation\Validator;
 
 include 'database.php';
-header("location: ../index.php");
+
+//header("location: ../index.php");
 $name = htmlspecialchars($_POST['name']);
 $firstname = htmlspecialchars($_POST['firstname']);
 $mail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$file = $_POST['file'];
+$file = "file";
+//$file = $_POST['file'];
 $description = htmlspecialchars($_POST['description']);
 
+$userInput = [
+    'name' => $name,
+    'firstname' => $firstname,
+    'mail' => $mail,
+    'file' => $file,
+    'description' => $description
+];
 
-$mail = filter_var($mail, FILTER_VALIDATE_EMAIL);
+$validator = new Validator;
+$validation = $validator->make($userInput, [
 
-$query = "INSERT INTO `contact`(`name`, `firstname`, `mail`, `file`, `description`) VALUES(?,?,?,?,?)";
-if (!empty($db)) {
-    $insert = $db->prepare($query);
-    $insert->execute(array(
-        $name,
-        $firstname,
-        $mail,
-        $file,
-        $description
-    ));
-    echo json_encode(
-        "['error' => 'error']"
-    );
-    exit();
+    'name' => 'required',
+    'firstname' => 'required',
+    'mail' => 'required|email',
+    'file' => '',//required|upload_file:png,jpeg
+    'description' => 'required'
+]);
+$validation->validate();
+
+if ($validation->fails()) {
+    $errors = $validation->errors();
+    echo "<pre>";
+    print_r($errors->firstOfAll());
+    echo "</pre>";
+    exit;
+} else {
+    $db = getDbconnection();
+    $query = "INSERT INTO `contact`(`name`, `firstname`, `mail`, `file`, `description`) VALUES(?,?,?,?,?)";
+    if (!empty($db)) {
+        $insert = $db->prepare($query);
+        $insert->execute(array(
+            $name,
+            $firstname,
+            $mail,
+            $file,
+            $description
+        ));
+//        echo json_encode(
+//            "['error' => 'error']"
+//        );
+        echo "add successfull";
+//        :
+        $db = null;
+        //exit();
+    }
+    echo "error";
 }
